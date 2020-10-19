cf2a90c6f168d6beb86328cc5af2cc30d3c47451
leejh-gitc <69024896+leejh-gitc@users.noreply.github.com>
2020-10-19 17:55:45 +0900

    Update and rename subnet.tf to nw.tf

---
+++ nw.tf
@@ -0,0 +1,84 @@
+# VPC
+resource "aws_vpc" "terra" {
+        cidr_block = "10.0.0.0/16"
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "intgw" {
+        vpc_id = aws_vpc.terra.id
+}
+
+# Subnet
+resource "aws_subnet" "pub-subnet1" {
+        vpc_id = aws_vpc.terra.id
+        cidr_block = "10.0.1.0/24"
+        availability_zone = "ap-northeast-2a"
+}
+
+resource "aws_subnet" "pub-subnet2" {
+        vpc_id = aws_vpc.terra.id
+        cidr_block = "10.0.2.0/24"
+        availability_zone = "ap-northeast-2c"
+}
+
+resource "aws_subnet" "pri-subnet1" {
+        vpc_id = aws_vpc.terra.id
+        cidr_block = "10.0.3.0/24"
+        availability_zone = "ap-northeast-2a"
+}
+
+resource "aws_subnet" "pri-subnet2" {
+        vpc_id = aws_vpc.terra.id
+        cidr_block = "10.0.4.0/24"
+        availability_zone = "ap-northeast-2c"
+}
+
+# Route Talbe
+resource "aws_route_table" "publicrt" {
+        vpc_id = aws_vpc.terra.id
+
+        route {
+                cidr_block = "0.0.0.0/0"
+                gateway_id = aws_internet_gateway.intgw.id
+                }
+}
+
+resource "aws_route_table" "privatert" {
+        vpc_id = aws_vpc.terra.id
+
+        route {
+                cidr_block = "0.0.0.0/0"
+                nat_gateway_id = aws_nat_gateway.gw.id
+                }
+}
+
+# Route Table association
+resource "aws_route_table_association" "pub1" {
+        subnet_id = aws_subnet.pub-subnet1.id
+        route_table_id = aws_route_table.publicrt.id
+}
+
+resource "aws_route_table_association" "pub2" {
+        subnet_id = aws_subnet.pub-subnet2.id
+        route_table_id = aws_route_table.publicrt.id
+}
+
+resource "aws_route_table_association" "pri1" {
+        subnet_id = aws_subnet.pri-subnet1.id
+        route_table_id = aws_route_table.privatert.id
+}
+
+resource "aws_route_table_association" "pri2" {
+        subnet_id = aws_subnet.pri-subnet2.id
+        route_table_id = aws_route_table.privatert.id
+}
+
+# EIP
+resource "aws_eip" "eipc" {
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "gw" {
+        allocation_id = aws_eip.eipc.id
+        subnet_id = aws_subnet.pub-subnet2.id
+}
--- subnet.tf
+++
@@ -1,7 +0,0 @@
-#AWS Subnet tf 
-provider "aws" {
-  region     = "ap-northeast-2"
-  access_key = var.uws-access
-  secret_key = var.uws-secret
-  }
-
